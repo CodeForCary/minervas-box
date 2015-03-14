b1a7c24a9833179e311fa512dc7bfedb5fb109f0
Juan Orozco <juanthedesigner@gmail.com>
2015-03-14 13:53:04 -0400

    Added art component to page.

--- src/pages/artwork/artwork.js
+++ src/pages/artwork/artwork.js
@@ -3,6 +3,8 @@ import 'canjs/stache';
 import template from './artwork.stache!';
 import viewmodel from './viewmodel';
 
+import 'components/art-photo/art-photo';
+
 can.Component.extend({
     tag: 'mp-artwork',
     template: template,
