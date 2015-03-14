9fa0d9bb8351197d41eb5961603b7595122a9369
Juan Orozco <juanthedesigner@gmail.com>
2015-03-14 10:29:58 -0400

    Added artwork model and fixture.

---
+++ src/models/artwork/artwork.js
@@ -0,0 +1,6 @@
+import can from 'canjs';
+import 'canjs/map.define';
+
+export default can.Model.extend({
+    findAll: 'GET /artwork.json'
+},{});
---
+++ src/models/artwork/fixture.js
@@ -0,0 +1,7 @@
+import can from 'canjs';
+import from 'canjs/fixture';
+
+
+export default can.fixture('GET /artwork.json', function() {
+    return [];
+});
