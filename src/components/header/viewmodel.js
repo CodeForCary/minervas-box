3c09e30f7614d6eae408cf9d1a5e353adf3b5cf9
Juan Orozco <juanthedesigner@gmail.com>
2015-04-20 09:44:15 -0400

    Improved routing and cleaned up code

--- src/components/header/viewmodel.js
+++ src/components/header/viewmodel.js
@@ -4,6 +4,6 @@ import 'can/map/define';
 export default can.Map.extend({
     define: {},
     getPageUrl: function(pageName) {
-        return can.route.url({page: pageName});
+        return '#!' + pageName;
     }
 });
