12f61f38c77f1cac3242309d53ff360befb0c0de
Juan Orozco <hi@juanorozco.com>
2016-07-12 23:40:51 -0400

    Improve ask permission component. Made component simpler, now relies on implementation to provide granted/denied templates via content. Using arrow functions.

--- src/components/ask-permission/ask-permission.js
+++ src/components/ask-permission/ask-permission.js
@@ -1,8 +1,10 @@
 import can from 'can';
 import 'can/view/stache/stache';
 import ViewModel from './ask-permission.viewmodel';
+import template from './ask-permission.stache!';
 
 can.Component.extend({
     tag: 'ma-ask-permission',
+    template: template,
     viewModel: ViewModel
 });
--- src/components/ask-permission/ask-permission.viewmodel.js
+++ src/components/ask-permission/ask-permission.viewmodel.js
@@ -2,5 +2,13 @@ import can from 'can';
 import 'can/map/define/define';
 
 export default can.Map.extend({
-    define: {}
+    define: {
+        formState: {
+            type: 'string',
+            value: 'FORM'
+        }
+    },
+    updateState: function (newState) {
+        this.attr('formState', newState);
+    }
 });
