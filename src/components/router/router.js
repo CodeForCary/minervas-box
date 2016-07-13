6e5bc7f59585703460783ede7457b306e9e986d6
Juan Orozco <hi@juanorozco.com>
2016-07-13 14:22:47 -0400

    Fix lint errors
    - Fixed ESLint/XO errors.

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,26 +1,26 @@
 import can from 'can';
 import 'can/view/stache/stache';
 import template from './router.stache!';
-import viewmodel from './viewmodel';
+import ViewModel from './viewmodel';
 
 can.Component.extend({
     tag: 'ma-router',
-    template: template,
-    scope: viewmodel,
+    template,
+    viewModel: ViewModel,
     events: {
-        inserted: function() {
-            var scope = this.scope,
-                routeKey = scope.attr('routeKey'),
-                routePath = scope.attr('routePath'),
-                routeTarget = scope.attr('routeTarget'),
-                defs = {};
+        inserted: () => {
+            const vm = this.viewModel;
+            const routeKey = vm.attr('routeKey');
+            const routePath = vm.attr('routePath');
+            const routeTarget = vm.attr('routeTarget');
+            const defs = {};
 
             if (routeKey) {
-                var routeKeys = routeKey ? routeKey.split('||') : [];
-                var routeTargets = routeTarget ? routeTarget.split('||'): [];
-                can.each(routeKeys, function (item, index) {
+                const routeKeys = routeKey ? routeKey.split('||') : [];
+                const routeTargets = routeTarget ? routeTarget.split('||') : [];
+                can.each(routeKeys, (item, index) => {
                     defs[item] = routeTargets[index] || null;
-                })
+                });
                 //path is same as target when path not explicitly passed
                 // if (typeof routePath === 'undefined') {
                 //     routePath = routeTargets[0];
--- src/components/router/viewmodel.js
+++ src/components/router/viewmodel.js
@@ -4,11 +4,11 @@ import 'can/map/define/define';
 export default can.Map.extend({
     define: {
         showRoute: {
-            get: function() {
-                var routePath = this.attr('routePath'),
-                    currentRoute = typeof can.route.attr('route') === 'undefined'? '': can.route.attr('route');
+            get: () => {
+                const routePath = this.attr('routePath');
+                const currentRoute = typeof can.route.attr('route') === 'undefined' ? '' : can.route.attr('route');
 
-                if ( currentRoute == routePath) {
+                if (currentRoute === routePath) {
                     return true;
                 }
                 return false;
